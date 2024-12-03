0b28a1e48a9abea7045b266eaf9dfb667272a82c
derickbessa <derickbessa12@gmail.com>
2024-12-03 13:44:18 -0300

    questao 3

---
+++ q3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define TAM 50
+
+int main(){
+    int contagem = 0;
+    char str[TAM];
+    printf("escreva uma palavra   \n");
+    fgets(str, sizeof(str),stdin);
+    for (int i = 0; i != '\0'; i++){
+        if(str[i] == '\n'){
+            str[i] = '\0';
+            break;
+        }
+    }
+
+    for (int i = 0; str[i] != '\0'; i++){
+        if(str[i] != '\n'){
+        contagem++; 
+    }}
+    printf("o numero de caracteres é %d\n", contagem);
+
+    return 0;
+}
